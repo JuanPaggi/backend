242454e36bf97a166a38d5d68e510ef961525c9b
Alexander <alexander.eberle@aizensoft.com>
2018-09-08 00:28:57 -0300

    Agregamos un ejemplo con parámetros.

--- backend/src/main/java/ar/com/tandilweb/byo/backend/Presentation/GQLServices/HelloWorldGQL.java
+++ backend/src/main/java/ar/com/tandilweb/byo/backend/Presentation/GQLServices/HelloWorldGQL.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import ar.com.tandilweb.byo.backend.Presentation.dto.out.Hola;
+import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.spqr.spring.annotation.GraphQLApi;
 
@@ -20,6 +21,13 @@ public class HelloWorldGQL {
 		Hola out = new Hola();
 		out.setMundo("Ok");
 		return out;
+	}
+	
+	@GraphQLQuery(name = "HelloWorldGQL_setUser")
+	public boolean setUser(@GraphQLArgument(name = "user") String usuario, @GraphQLArgument(name = "password") String pass) {
+		log.info("Usuario: " + usuario);
+		log.debug("Password: " + pass);
+		return true;
 	}
 
 }
