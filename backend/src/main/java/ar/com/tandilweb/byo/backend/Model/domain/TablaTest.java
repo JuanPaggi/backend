4b4af74cfab611b07c63427cfb5633e8b32acd3f
Alexander <alexander.eberle@aizensoft.com>
2018-09-07 21:57:06 -0300

    Capa de modelo de datos

---
+++ backend/src/main/java/ar/com/tandilweb/byo/backend/Model/domain/TablaTest.java
@@ -0,0 +1,33 @@
+package ar.com.tandilweb.byo.backend.Model.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//https://www.tutorialspoint.com/es/jpa/jpa_orm_components.htm
+@Entity
+public class TablaTest {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id_tabla;
+	private String namejaja;
+	
+	public Integer getId_tabla() {
+		return id_tabla;
+	}
+
+	public void setId_tabla(Integer id_tabla) {
+		this.id_tabla = id_tabla;
+	}
+
+	public String getNamejaja() {
+		return namejaja;
+	}
+
+	public void setNamejaja(String namejaja) {
+		this.namejaja = namejaja;
+	}
+	
+}
---
+++ backend/src/main/java/ar/com/tandilweb/byo/backend/Model/repository/TablaTestRepository.java
@@ -0,0 +1,11 @@
+package ar.com.tandilweb.byo.backend.Model.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.com.tandilweb.byo.backend.Model.domain.TablaTest;
+
+public interface TablaTestRepository extends CrudRepository<TablaTest, Integer> {
+//	Employee findByFirstName(String firstName);
+//	List<Employee> findByLastName(String lastName);
+}
+
