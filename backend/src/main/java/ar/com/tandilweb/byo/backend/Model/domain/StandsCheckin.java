b4d5f6da93f71b543bf26f42e71c246b093668cc
Alexander <alexander.eberle@aizensoft.com>
2018-09-20 20:10:22 -0300

    Correccion errors hibernate.

--- backend/src/main/java/ar/com/tandilweb/byo/backend/Model/domain/StandsCheckin.java
+++ backend/src/main/java/ar/com/tandilweb/byo/backend/Model/domain/StandsCheckin.java
@@ -1,43 +1,50 @@
 package ar.com.tandilweb.byo.backend.Model.domain;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 @Entity
-public class StandsCheckin {
+public class StandsCheckin implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@NotNull
+	Long id_stand;
 	
+	@Id
 	@NotNull
-	Stands id_stand;
-	@NotNull
-	Users id_user;
+	Long id_user;
 	
 	@NotNull
 	Date date_checkin;
 
-	public Stands getId_stand() {
-		return id_stand;
-	}
-
-	public void setId_stand(Stands id_stand) {
-		this.id_stand = id_stand;
-	}
-
-	public Users getId_user() {
-		return id_user;
-	}
-
-	public void setId_user(Users id_user) {
-		this.id_user = id_user;
-	}
-
-	public Date getDate_checkin() {
-		return date_checkin;
-	}
-
-	public void setDate_checkin(Date date_checkin) {
-		this.date_checkin = date_checkin;
-	}
+//	public Stands getId_stand() {
+//		return id_stand;
+//	}
+//
+//	public void setId_stand(Stands id_stand) {
+//		this.id_stand = id_stand;
+//	}
+//
+//	public Users getId_user() {
+//		return id_user;
+//	}
+//
+//	public void setId_user(Users id_user) {
+//		this.id_user = id_user;
+//	}
+//
+//	public Date getDate_checkin() {
+//		return date_checkin;
+//	}
+//
+//	public void setDate_checkin(Date date_checkin) {
+//		this.date_checkin = date_checkin;
+//	}
 	
 }
