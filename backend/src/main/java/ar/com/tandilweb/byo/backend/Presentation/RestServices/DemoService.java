2f72c6d1c9e4a85bf14af6e871c79cefe70024ae
Alexander <alexander.eberle@aizensoft.com>
2018-10-13 16:26:38 -0300

    Rest demo para probar el deploy.

--- backend/src/main/java/ar/com/tandilweb/byo/backend/Presentation/RestServices/DemoService.java
+++ backend/src/main/java/ar/com/tandilweb/byo/backend/Presentation/RestServices/DemoService.java
@@ -1,10 +1,15 @@
 package ar.com.tandilweb.byo.backend.Presentation.RestServices;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/demoService")
 public class DemoService {
-
+	
+	@RequestMapping(path="/info", method=RequestMethod.GET) 
+	public String informationService() {
+		return "hola mundo";
+	}
 }
