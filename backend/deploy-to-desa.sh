039ee03a07c63c60950a7df254b4e65f505a1ab0
Alexander Eberle <alexander171294@gmail.com>
2018-10-28 12:26:56 -0300

    Fix deploy script

--- backend/deploy-to-desa.sh
+++ backend/deploy-to-desa.sh
@@ -1,4 +1,4 @@
 mvn clean package
 cd target
 scp byo-alpha.war tomcatdeploy@144.76.43.230:/home/tomcatdeploy/buscoyofrezco.war
-ssh tomcatdeploy@144.76.43.230 -f /home/tomcatdeploy/deploy-byo.sh
+ssh tomcatdeploy@144.76.43.230 /home/tomcatdeploy/deploy-byo.sh
