cc07ec9c1060fc67c5685f45babc84815985a482
Alexander Eberle <alexander171294@gmail.com>
2018-11-03 20:00:37 -0300

    removemos repositorios

--- backend/src/main/java/ar/com/tandilweb/byo/backend/Model/repository/TablaTestRepository.java
+++
@@ -1,7 +0,0 @@
-package ar.com.tandilweb.byo.backend.Model.repository;
-
-public interface TablaTestRepository{ // extends CrudRepository<TablaTest, Integer> {
-//	Employee findByFirstName(String firstName);
-//	List<Employee> findByLastName(String lastName);
-}
-
