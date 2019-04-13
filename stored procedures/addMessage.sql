9d42f4df895098590e7cdc72017faa5da92ad7ed
Ezequiel Fazio <ezequiel.tandilweb@gmail.com>
2019-04-13 14:17:15 +0000

    funcionando

--- stored procedures/addMessage.sql
+++ stored procedures/addMessage.sql
@@ -26,12 +26,12 @@ BEGIN
         INSERT INTO mensajes(id_sender, id_target, messages, fecha, is_viewed) 
             VALUES(p_id_sender, p_id_target, p_message, p_fecha, FALSE);
         SET out_message_id = LAST_INSERT_ID();
-        SELECT count(last_message_id) INTO chatIniciado FROM chats WHERE (id_user_requester = p_id_sender AND id_user_sender = p_id_target) OR (id_user_requester = p_id_target AND id_user_sender = p_id_sender);
+        SELECT count(last_message_id) INTO chatIniciado FROM chats WHERE (id_user_sender = p_id_target AND id_user_requester = p_id_sender) OR (id_user_requester = p_id_target AND id_user_sender = p_id_sender);
 		IF chatIniciado > 0 THEN 
-			UPDATE chats SET last_message_id = out_message_id WHERE (id_user_requester = p_id_sender AND id_user_sender = p_id_target) OR (id_user_requester = p_id_target AND id_user_sender = p_id_sender);
+			UPDATE chats SET last_message_id = out_message_id WHERE (id_user_sender = p_id_target AND id_user_requester = p_id_sender) OR (id_user_requester = p_id_target AND id_user_sender = p_id_sender);
 		ELSE
 			INSERT INTO chats(id_user_requester, id_user_sender, last_message_id) 
-                        VALUES(p_id_target, p_id_sender, out_message_id);
+                        VALUES(p_id_sender, p_id_target, out_message_id);
 		END IF;
     ELSE
         SELECT mensajes, chats INTO totalMensajes, totalChats FROM interacciones WHERE id_user = p_id_sender;
@@ -40,15 +40,27 @@ BEGIN
             SET out_ecode = 1;
             SET out_emsg = 'LIMITE DE MENSAJES';
         ELSE
-            SELECT count(last_message_id) INTO chatIniciado FROM chats WHERE (id_user_requester = p_id_sender AND id_user_sender = p_id_target) OR (id_user_requester = p_id_target AND id_user_sender = p_id_sender);
+        
+        INSERT INTO desa_byo.interacciones(id_user, mensajes, solicitudes, chats)
+				VALUES (p_id_sender, 1, 0, 0)
+			ON DUPLICATE KEY UPDATE
+			mensajes = mensajes+1;
+
+            SELECT count(last_message_id) INTO chatIniciado FROM chats WHERE(id_user_sender = p_id_target AND id_user_requester = p_id_sender) OR (id_user_requester = p_id_target AND id_user_sender = p_id_sender);
             if chatIniciado > 0 THEN 
+             SELECT valor INTO maxChats FROM general_configurations WHERE id = 6;
+                IF totalChats >= maxChats THEN
+                    SET out_ecode = 2;
+                    SET out_emsg = 'LIMITE DE CHATS';
+                    
+                ELSE
                 INSERT INTO mensajes(id_sender, id_target, messages, fecha, is_viewed) 
                     VALUES(p_id_sender, p_id_target, p_message, p_fecha, FALSE);
                 SET out_message_id = LAST_INSERT_ID();
-                UPDATE chats SET last_message_id = out_message_id WHERE (id_user_requester = p_id_sender AND id_user_sender = p_id_target) OR (id_user_requester = p_id_target AND id_user_sender = p_id_sender);
-                UPDATE interacciones SET mensajes = mensajes + 1 WHERE id_user = p_id_sender;
+                UPDATE chats SET last_message_id = out_message_id WHERE (id_user_sender = p_id_target AND id_user_requester = p_id_sender) OR (id_user_requester = p_id_target AND id_user_sender = p_id_sender);
+				END IF;
             ELSE
-                SELECT valor INTO maxChats FROM general_configurations WHERE id = 7;
+                SELECT valor INTO maxChats FROM general_configurations WHERE id = 6;
                 IF totalChats >= maxChats THEN
                     SET out_ecode = 2;
                     SET out_emsg = 'LIMITE DE CHATS';
@@ -57,7 +69,7 @@ BEGIN
                     VALUES(p_id_sender, p_id_target, p_message, p_fecha, FALSE);
                     SET out_message_id = LAST_INSERT_ID();
                     INSERT INTO chats(id_user_requester, id_user_sender, last_message_id) 
-                        VALUES(p_id_target, p_id_sender, out_message_id);
+                        VALUES(p_id_sender, p_id_target, out_message_id);
                     UPDATE interacciones SET mensajes = mensajes + 1, chats = chats + 1 WHERE id_user = p_id_sender;
                 END IF;
             END IF;
